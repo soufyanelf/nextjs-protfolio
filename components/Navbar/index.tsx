7bbc295dc57df164e03e46c69443220560132ce5
Ismail Boularbah <qiranorane55@gmail.com>
2022-11-12 14:42:23 +0100

    Navbar Component | Light/Dark Theme Switch Functionality

---
+++ components/Navbar/index.tsx
@@ -0,0 +1,35 @@
+import Link from 'next/link';
+import { FunctionComponent } from 'react';
+import ThemeToggler from '../../src/ThemeToggler';
+
+const Navbar: FunctionComponent = () => {
+  return (
+    <nav className='flex justify-between items-center sm:flex-row flex-col gap-4'>
+        <div className='flex justify-center items-center gap-2'>
+            <h2 className='text-xl font-bold' title="Ismailium - Layhdi Chabab :)">
+                <Link href="/">
+                    Ismailium
+                </Link>
+            </h2>
+            <div>
+                <ThemeToggler />
+            </div>
+        </div>
+        <div>
+            <ul className='flex justify-center items-center gap-6'>
+                <li className='font-semibold text-black text-sm dark:text-gray-300' title="About">
+                    <Link href="">About</Link>
+                </li>
+                <li className='font-semibold text-black text-sm dark:text-gray-300' title="Projects">
+                    <Link href="">Projects</Link>
+                </li>
+                <li className='font-semibold text-black text-sm dark:text-gray-300' title="Get in touch">
+                    <Link href="">Get in touch</Link>
+                </li>
+            </ul>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,18 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 
+import Navbar from '../components/Navbar';
+
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className='max-w-6xl w-100 m-auto mt-4'>
       <Head>
         <title>Ismailium - Layhdi Chabab</title>
         <link rel="icon" href="/Fez-Hat.png" />
       </Head>
 
-      <main className='-4 flex justify-center items-center min-h-screen'>
-        <h1 className='text-xl font-bold'>
-          Ismailium - Layhdi Chabab
-        </h1>
+      <main className='p-4'>
+        <Navbar />
       </main>
     </div>
   )
