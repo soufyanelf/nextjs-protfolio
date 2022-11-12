0e20bb5935cb70cbd5c130e7b173927b250de731
Ismail Boularbah <qiranorane55@gmail.com>
2022-11-12 23:03:37 +0100

    Current Year : Static value => Dynamic value

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 const Footer = () => {
+  const now = new Date();
+  const year = now.getFullYear();
   return (
     <footer className='absolute left-0 bottom-6 flex justify-center items-center w-full m-0'>
         <h5 className='text-black dark:text-gray-300 text-center sm:text-sm text-xs'>
-            &copy; 2022 - Made with ❤️ by <b>Ismailium</b>
+            &copy; {year} - Made with ❤️ by <b>Ismail Boularbah</b>
         </h5>
     </footer>
   )
