777d1d0041dc5520b05b3fcb232db6cfc85ba471
Soufyan <sfoukahi@gmail.com>
2023-01-15 19:42:39 -0500

    adding blog

---
+++ pages/blog/index.tsx
@@ -0,0 +1,53 @@
+import fs from 'fs';
+import matter from 'gray-matter';
+import Image from 'next/image';
+import Link from 'next/link';
+
+
+export async function getStaticProps() {
+    const files = fs.readdirSync('./content/blog/posts');
+    const posts = files.map((fileName) => {
+
+        const slug = fileName.replace('.md', '');
+        const readFile = fs.readFileSync(`./content/blog/posts/${fileName}`, 'utf-8');
+        const { data: frontmatter } = matter(readFile);
+
+        return {
+            slug,
+            frontmatter,
+        };
+    });
+
+
+    return {
+        props: {
+            posts,
+        }
+    }
+}
+
+
+
+
+export default function Home({ posts }) {
+    return (
+      <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 p-4 md:p-0'>
+        {posts.map(({ slug, frontmatter }) => (
+          <div
+            key={slug}
+            className='border border-gray-200 m-2 rounded-xl shadow-lg overflow-hidden flex flex-col'
+          >
+            <Link href={`/blog/${slug}`}>
+                <Image
+                  width={650}
+                  height={340}
+                  alt={frontmatter.title}
+                  src={`/${frontmatter.socialImage}`}
+                />
+                <h1 className='p-4'>{frontmatter.title}</h1>
+            </Link>
+          </div>
+        ))}
+      </div>
+    );
+  }
